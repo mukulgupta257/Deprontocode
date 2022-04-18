0a238e1aebea35319ccc3467d1bba28d70972bf3
Mukul Gupta <mukulgupta257@gmail.com>
2022-04-18 19:05:20 +0530

    filter added

--- table/index.js
+++ table/index.js
@@ -9,29 +9,71 @@ const getdata=async _=>{
 return await posts.json()
 }
 
-const TableScreen={
+const Completed={
     render:async _=>{
         const tabledata=await getdata()
         return`
         <div>
         <p>
-        <button>Sort</button>
+        <button id="completed">Show completed</button>
         </P>
-            <table>
+            <table id="dataTabe">
                 <thead>
                     <tr>
-                        <td>Userid</td>
+                        <td>User Id</td>
                         <td>ID</td>
-                        <td>Title</td>
+                        <td colspan=2>Title</td>
                         <td>Status</td>
                     </tr>
                 </thead>
                 <tbody>
                     ${tabledata.map(data=>`
+                    ${data.completed===true?`<tr>
+                    <td>1</td>
+                    <td>${data.id}</td>
+                    <td colspan=2>${data.title}</td>
+                    <td>${data.completed}</td>
+                </tr>`:``}
+                    
+                    }
+                    `).join("\n")}
+                </tbody>
+            </table>
+        </div>
+        `
+    }
+}
+
+const TableScreen={
+    after_render:async _=>{
+        document.getElementById('completed').addEventListener('click',async _=>{
+            const main_div=document.getElementById('main-div')
+            main_div.innerHTML=await Completed.render();
+        })
+    },
+    render:async _=>{
+        const tabledata=await getdata()
+        return`
+        <div>
+        <p>
+        <button id="completed">Show completed</button>
+        </P>
+            <table id="dataTabe">
+                <thead>
+                    <tr>
+                        <td>User Id</td>
+                        <td>ID</td>
+                        <td colspan=2>Title</td>
+                        <td>Status</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${tabledata.map(data=>`
+                    
                     <tr>
                         <td>${data.userId}</td>
                         <td>${data.id}</td>
-                        <td>${data.title}</td>
+                        <td colspan=2>${data.title}</td>
                         <td>${data.completed}</td>
                     </tr>
                     `).join("\n")}
